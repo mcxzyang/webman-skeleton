bb92e9398455f39e0d1139e9af7fc32bb5629897
mcxzyang <mcxzyang@gmail.com>
2024-03-14 20:43:42 +0800

    Webhook

--- app/controller/HookController.php
+++ app/controller/HookController.php
@@ -2,7 +2,6 @@
 
 namespace app\controller;
 
-use support\Log;
 use support\Request;
 
 class HookController
@@ -14,17 +13,9 @@ class HookController
         $key = 'cherrybeal';
         $hashedData = hash_hmac('sha1', $body, $key);
         if (hash_equals('sha1='.$hashedData, $codingSign)) {
-//            $target = '/var/www/skeleton/';
              $cmd = 'sudo -Hu www-data git pull';
-             $result = shell_exec($cmd. ' 2>&1');
-             return $result;
-//            return shell_exec($cmd);
+             return shell_exec($cmd. ' 2>&1');
         }
-    }
-
-    public function test()
-    {
-        $cmd = 'sudo -Hu www-data git pull';
-        return shell_exec($cmd. ' 2>&1');
+        return null;
     }
 }
